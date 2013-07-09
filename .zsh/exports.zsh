04a52ed26c8ba2937a163e3f412bc73825b9fdf7
Jan Antala <jan@antala.sk>
2013-07-09 03:01:01 +0200

    zsh

---
+++ .zsh/exports.zsh
@@ -0,0 +1,14 @@
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+export PATH=${PATH}:/Users/janantala/android-sdk-macosx/platform-tools/:/Users/janantala/android-sdk-macosx/tools/
+
+export PATH=/usr/local/texlive/2011/bin:$PATH
+
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
+export PATH="/usr/local/heroku/bin:$PATH"
+
+export PROMPT_COMMAND='echo -ne "\033]0;[${PWD/$HOME/~}]\007"'
---
+++ .zshrc
@@ -0,0 +1,5 @@
+source ~/.zsh/setopt.zsh
+source ~/.zsh/exports.zsh
+source ~/.zsh/prompt.zsh
+source ~/.zsh/history.zsh
+source ~/.zsh/functions.zsh
