f8cdd455ba1aba38ca55e695f14bfe5804ed0cf5
Jan Antala <jan@antala.sk>
2013-07-25 22:03:43 +0200

    function clone git repo, cd into it and open in sublime tex

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,3 +1,5 @@
+# make gif image from .mov file
+
 gifify() {
   if [[ -n "$1" ]]; then
     if [[ $2 == '--good' ]]; then
@@ -10,4 +12,47 @@ gifify() {
   else
     echo "proper usage: gifify <input_movie.mov>. You DO need to include extension."
   fi
+}
+
+
+# clones a repository, cds into it, and opens it in my editor.
+# - arg 1 - url|username|repo remote endpoint, username on github, or name of
+#           repository.
+# - arg 2 - (optional) name of repo
+#
+# usage:
+#   $ clone things
+#     .. git clone git@github.com:stephenplusplus/things.git things
+#     .. cd things
+#     .. subl .
+#
+#   $ clone git@github.com:stephenplusplus/dots.git
+#     .. git clone git@github.com:stephenplusplus/dots.git dots
+#     .. cd dots
+#     .. subl .
+#
+#   $ clone yeoman generator
+#     .. git clone git@github.com:yeoman/generator.git generator
+#     .. cd generator
+#     .. subl .
+
+function clone {
+  local url=$1;
+  local repo=$2;
+
+  if [[ ${url:0:4} == 'http' || ${url:0:3} == 'git' ]]
+  then
+    # just clone this thing.
+    repo=$(echo $url | awk -F/ '{print $NF}' | sed -e 's/.git$//');
+  elif [[ -z $repo ]]
+  then
+    # my own stuff.
+    repo=$url;
+    url="git@github.com:stephenplusplus/$repo";
+  else
+    # not my own, but I know whose it is.
+    url="git@github.com:$url/$repo.git";
+  fi
+
+  git clone $url $repo && cd $repo && subl .;
 }
