f9ed839eebca48a29242ff83bf28d2ddeea8ede9
Jan Antala <jan@antala.sk>
2014-07-07 22:49:01 +0200

    feat: add gradle home

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -12,3 +12,7 @@ export PATH="/usr/local/heroku/bin:$PATH"
 export PROMPT_COMMAND='echo -ne "\033]0;[${PWD/$HOME/~}]\007"'
 
 export ANDROID_HOME=/Applications/Android\ Studio.app/sdk/
+
+GRADLE_HOME=/Users/janantala/gradle;
+export GRADLE_HOME
+export PATH=$PATH:$GRADLE_HOME/bin
